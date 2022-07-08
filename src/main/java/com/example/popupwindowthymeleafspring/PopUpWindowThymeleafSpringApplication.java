d5fb99cb7a23d971e13938a1d72097e693f0d6cc
Csaba79-coder <csabavadasz79@gmail.com>
2022-07-08 16:58:56 +0200

    Initial commit

---
+++ src/main/java/com/example/popupwindowthymeleafspring/PopUpWindowThymeleafSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.popupwindowthymeleafspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopUpWindowThymeleafSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopUpWindowThymeleafSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/popupwindowthymeleafspring/PopUpWindowThymeleafSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popupwindowthymeleafspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopUpWindowThymeleafSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
